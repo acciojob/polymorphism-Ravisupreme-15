71e9154103ec68b6de5b51bdde4dd974fc8faef9
RaviSupreme-15 <supremeravi1819@gmail.com>
2023-06-09 16:30:23 +0530

    commit changes observed polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         public int product(int a, int b){
 
-            return a+b;
+            return a*b;
 
         }
 
@@ -25,10 +25,6 @@ public class Main {
 
 
     }
-
-
-
-
     public static void main(String args[]){
 
 
@@ -38,13 +34,13 @@ public class Main {
 
 
         // int a , int b
-        System.out.println("sum of 5 and 6 is "+product.product(5,6));
+        System.out.println( "product of 5 and 6 is "+product.product(5,6));
 
         // int a, int b, int c
         System.out.println("product of 9,8 and 8 is "+ product.product(9,8,7));
 
         // double a, double b
-        System.out.println("product of 10000.0 and 39999.0 is "+ product.product(10000.0,39999.0));
+        System.out.println("product of 100.00 and 399.00 is "+ product.product(100.00,399.00));
 
 
 
